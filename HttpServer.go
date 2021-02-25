8f8150dc472003d6bd48ccb9522d16e3acf0a99f
AjinkyaBadiger <ajinkyabadiger@gmail.com>
2021-02-25 15:18:24 +0000

    Removed unused function

--- HttpServer.go
+++ HttpServer.go
@@ -10,13 +10,6 @@ import (
 
 type HttpHandler struct  {}
 
-func (hh HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	//Create a response binary data
-	data := []byte("Hello World!\n")  // slice of bytes
-        fmt.Println(req.URL)
-	res.Write(data)
-}
-
 func main() {
 
 
